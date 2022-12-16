dedbb9aa29cc5c8b4526310353bd4cc6745bb62f
Sreejith Nair <76815771+sreejithnair190@users.noreply.github.com>
2022-12-16 12:13:26 +0530

    Delete new.php

--- new.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "test2");
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$sql = "INSERT INTO users('fname','lname','email','phone') VALUES('$fname','$lname','$email',$phone)";
-mysqli_query($conn, $sql);
-?>
